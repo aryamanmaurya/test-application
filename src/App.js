c60a4d41c18dbf7c6ed61d19535eea85a91548c1
arya <aryamanmaurya29@gmail.com>
2024-10-12 19:13:40 +0000

    version3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my demo project, Version2.
+          This is my demo project, Version3.
         </p>
         <a
           className="App-link"
